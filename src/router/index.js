0fd99ea3516569a9ac5f1ef11287fb01adb816b6
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-03-17 14:31:50 +0100

    :UPDATE in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,3 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import GithubRepos from '../components/GithubRepos.vue'
-import ErrorPage from '../components/ErrorPage.vue'
-import GithubHome from '../components/GithubHome.vue'
-import EachRepo from '../components/EachRepo.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: GithubHome
-    },
-    {
-      path: '/YoungTechie/Repositories',
-      name: 'GithubRepos',
-      component: GithubRepos
-    },
-    {
-      path: '/YoungTechie/repo/:repoName',
-      name: 'EachRepo',
-      component: EachRepo,
-    },
-    { path: '/404', name: 'page-no-found', component: ErrorPage},
-    { path: '/:catchAll(.*)', redirect: '/404'}
-    ]
-})
-
-export default router
 // Import the necessary functions from the Vue Router library and the components to be used in the routes
 import { createRouter, createWebHistory } from 'vue-router'
 import GithubRepos from '../components/GithubRepos.vue'
