c2d0c75ad6f8a09d485459c5c7cf9d74f90637ff
Olaegbe Abdul-Rahmon <105429445+Youngtechie@users.noreply.github.com>
2023-03-17 10:20:41 +0100

    Update ErrorPage.vue

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -1,6 +1,27 @@
 <template>
-    <h1>404 Not Found</h1>
+    <section className="errorPage">
+            <section className='error'><span>404</span> <span>error</span></section>
+
+            <section className='pageE'>This page doesn't exist</section>
+
+            <RouterLink to="/"><button :class="{ isDark: store.isDark }">Go Home</button></RouterLink>
+    </section>
 </template>
 <script setup>
 </script>
-<style></style>
+<style scoped>
+.errorPage{
+    height: 100vh;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    font-family: cursive;
+}
+.error{
+    font-size: 50px;
+}
+.pageE{
+    margin-bottom: 50px;
+}
+</style>
